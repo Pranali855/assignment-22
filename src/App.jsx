4784409d12ab65e2be616fc6cdf957bf41e34c0e
pranali <kokatepranu833@gmail.com>
2026-02-13 15:04:03 +0530

    Update App.jsx
    code added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import CardList from "./Component/CardList.jsx";
+
+export default function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}> Assignment -22</h1>
+      <CardList />
+    </div>
+  );
+}
